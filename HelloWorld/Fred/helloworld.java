725bfbfd77ac35d3fbf302345dead6e509a1c39e
frederickhaggins <cc1771@wayne.edu>
2017-01-28 23:11:08 -0500

    Java Hello World

--- HelloWorld/Fred/helloworld.java
+++ HelloWorld/Fred/helloworld.java
@@ -2,8 +2,7 @@
  * Created by frederickhaggins on 1/27/17.
  */
 public class helloworld {
-    public static void main(String []args)
-    {
+    public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
